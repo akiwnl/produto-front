974db10d6a76537c0d6c68818c40dac193ab5a63
akiwnl <carvalhokaike69@gmail.com>
2024-11-05 00:08:28 -0300

    first commit

---
+++ lib/data/dummy_products.dart
@@ -0,0 +1,34 @@
+const DUMMY_PRODUCT = {
+  "products": [
+    {
+      "id": "1",
+      "title": "Product 1",
+      "description": "Description of Product 1",
+      "price": 100,
+    },
+    {
+      "id": "1",
+      "title": "Product 1",
+      "description": "Description of Product 1",
+      "price": 100,
+    },
+    {
+      "id": "1",
+      "title": "Product 1",
+      "description": "Description of Product 1",
+      "price": 100,
+    },
+    {
+      "id": "1",
+      "title": "Product 1",
+      "description": "Description of Product 1",
+      "price": 100,
+    },
+    {
+      "id": "1",
+      "title": "Product 1",
+      "description": "Description of Product 1",
+      "price": 100,
+    },
+  ]
+};

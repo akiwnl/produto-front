f2c7e3a3ce63d4540193f000911b509138bd4297
akiwnl <carvalhokaike69@gmail.com>
2024-11-05 01:18:15 -0300

    create product entity

---
+++ lib/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  String id;
+  String description;
+  double price;
+  int quantity;
+
+  Product({
+    required this.id,
+    required this.description,
+    required this.price,
+    required this.quantity,
+  });
+}

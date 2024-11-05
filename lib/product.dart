bf68ba54838b4444c447788fdf55b005b6883137
akiwnl <carvalhokaike69@gmail.com>
2024-11-05 11:57:17 -0300

    add factory fromJson

--- lib/product.dart
+++ lib/product.dart
@@ -1,13 +1,21 @@
 class Product {
-  String id;
-  String description;
-  double price;
-  int quantity;
+  final String description;
+  final double price;
+  final int quantity;
 
   Product({
-    required this.id,
     required this.description,
     required this.price,
     required this.quantity,
   });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      description: json['description'],
+      price: (json['price'] is String)
+          ? double.parse(json['price'])
+          : json['price'].toDouble(),
+      quantity: json['quantity'],
+    );
+  }
 }

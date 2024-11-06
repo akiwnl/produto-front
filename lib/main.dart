ad6da5ccf015b179d786b3adb51a73d667267cf5
akiwnl <carvalhokaike69@gmail.com>
2024-11-06 19:39:55 -0300

    add debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Product CRUD',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
